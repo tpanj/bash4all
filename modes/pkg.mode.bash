434b68a84b853c7685bbd90e5e59bc90976b9378
Tadej Panjtar <tadej.panjtar@gmail.com>
2024-03-24 12:07:07 +0100

    Add Pkg and updated ARCH

--- modes/pkg.mode.bash
+++ modes/pkg.mode.bash
@@ -1,63 +1,54 @@
 # A-Z Sorted. Alias content always in single quotes
-unalias I i U u S se Qf q 2>/dev/null
+unalias I i Qf q S se U u 2>/dev/null
 
 if [ ARCH = "$BASED" ]; then
-alias I='pacman -Syu'
-alias i='pacman -S'
-alias U='pacman -Rys'
-alias u='pacman -Rs'
-alias se='pacman -Ss'
-alias Qf='pacman -Fl'
+alias I='sudo pacman -Syu --needed'
+alias i='sudo pacman -S --needed'
+alias Pkg='makepkg -fp PKGBUILD'
+alias Qf='sudo pacman -Fyl'
 alias q='pacman -Qi'
+alias se='pacman -Ss'
+alias U='sudo pacman -Rys'
+alias u='sudo pacman -Rs'
 fi
 
 if [ BEOS = "$BASED" ]; then
 alias I='pkgman install -y'
 alias i='pkgman install'
+alias se='pkgman search'
 alias U='pkgman uninstall -y'
 alias u='pkgman uninstall'
-alias se='pkgman search'
 fi
 
 if [ BSD = "$BASED" ]; then
 alias I='sudo pkg install -y'
 alias i='sudo pkg install'
+alias q='pkg info'
+alias se='pkg search'
 alias U='sudo pkg remove -y'
 alias u='sudo pkg remove'
-alias se='pkg search'
-alias q='pkg info'
 fi
 
 if [ DEBIAN = "$BASED" ]; then
 alias I='sudo apt-get -y install'
 alias i='sudo apt-get install'
-alias U='sudo apt-get -y remove'
-alias u='sudo apt-get remove'
-alias S='sudo service'
-alias se='apt search'
+alias Pkg='dpkg-buildpackage --no-sign'
 alias Qf='apt-file search'
 alias q='apt show'
-fi
-
-if [ REDHAT = "$BASED" ]; then
-alias I='sudo dnf install -y'
-alias i='sudo dnf install'
-alias U='sudo dnf remove -y'
-alias u='sudo dnf remove'
-alias Qf='dnf provides'
-alias q='dnf info'
 alias S='sudo service'
-alias se='dnf info'
+alias se='apt search'
+alias U='sudo apt-get -y remove'
+alias u='sudo apt-get remove'
 fi
 
 if [ GENTOO = "$BASED" ]; then
 alias I='emerge -a'
 alias i='emerge'
-alias U='emerge -avc'
-alias u='emerge -vc'
-alias se='emerge -S'
 alias Qf='pfl'
 alias q='emerge -S'
+alias se='emerge -S'
+alias U='emerge -avc'
+alias u='emerge -vc'
 fi
 
 if [ MAC = "$BASED" ]; then
@@ -68,30 +59,42 @@ fi
 if [ MAGEIA = "$BASED" ]; then
 alias I='sudo urpmi --no-recommends --auto'
 alias i='sudo urpmi --no-recommends'
-alias U='sudo urpme --auto'
-alias u='sudo urpme'
-alias S='sudo service'
-alias se='urpmf --summary' # outputs package:summary
+alias Pkg='rpmbuild -bb'
 alias Qf='urpmf' # outputs package:file
 alias q='urpmq -i'
+alias S='sudo service'
+alias se='urpmf --summary' # outputs package:summary
+alias U='sudo urpme --auto'
+alias u='sudo urpme'
+fi
+
+if [ REDHAT = "$BASED" ]; then
+alias I='sudo dnf install -y'
+alias i='sudo dnf install'
+alias Qf='dnf provides'
+alias q='dnf info'
+alias S='sudo service'
+alias se='dnf info'
+alias U='sudo dnf remove -y'
+alias u='sudo dnf remove'
 fi
 
 if [ SUSE = "$BASED" ]; then
 alias I='sudo zypper install -y'
 alias i='sudo zypper install'
-alias U='zypper remove -y'
-alias u='zypper remove'
-alias S='sudo service'
-alias se='zypper search'
 alias Qf='zypper info'
 alias q='zypper what-provides'
+alias S='sudo service'
+alias se='zypper search'
+alias U='zypper remove -y'
+alias u='zypper remove'
 fi
 
 # autocomplete aliases
 if [ -f ~/.bash4all/core/bash_completion.sh ]; then
   type _complete_alias >/dev/null 2>&1
   if [ $? = 1 ]; then . ~/.bash4all/core/bash_completion.sh; fi
-  for c in I i U u S se Qf q; do
+  for c in I i Qf q S se U u; do
     complete -F _complete_alias $c
   done
 fi
